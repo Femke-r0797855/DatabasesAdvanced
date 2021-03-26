52027a076d7b29207866fcb4490459cfcca53e2b
Femke-r0797855 <79083840+Femke-r0797855@users.noreply.github.com>
2021-03-26 14:00:05 +0100

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -2,15 +2,14 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import pymongo as mongo
+import redis
 
-#Database aanmaken
+#MongoDB aanmaken
 client = mongo.MongoClient("mongodb://127.0.0.1:27017") #Connect to mongo db
-
 db_BitCoin = client["BitCoinTransactie"]
 col_BitCoin = db_BitCoin["BitCoin"]
 
-#File maken als deze niet bestaat
-#f = open("LogBitCoinMost.txt", "w")
+
 #Scaper
 def scrape():
   url = 'https://www.blockchain.com/btc/unconfirmed-transactions'
@@ -19,48 +18,35 @@ def scrape():
 
   naam = soup.find_all('a', attrs={'class': 'sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK'})
   amount = soup.find_all('span', attrs={'class': 'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
+
+  #Data name
   for tag in range(len(naam)):
     #print(tag.text.strip())
-    datam.append(naam[tag].text[1:])
+    datan.append(naam[tag].text[1:])
     #print(amount[x].text.strip())
 
+ #Data Amount
   for tag in range(len(amount)):
     if amount[tag].text.find('$') ==0:
       dataa.append(amount[tag].text[1:])
-      #print(amount)
-    
-  highest(datam, dataa)
   
+  #Data Bitcoin
+  for tag in range(len(amount)):
+    if amount[tag].text.find('BTC') > 0:
+      datab.append(amount[tag].text[1:])
 
-def highest(naam, amount):
+  cach(datan, dataa, datab)
+  
+#Caching in Redis
+def cach(naam, amount):
   if len(naam) == 0: #Dit is omdat ik soms een index out of range kreeg ik weet ook nie hoe maar nu gaat die da opnieuw proberen zodat die shit wel doet
     scrape()
-  #print(len(naam))
-  #print(len(amount))
-  h = 0
-  p = -1
-  a = 'koeloekoeloe'
-  for x in range(len(amount)):
-    q = amount[x].replace(',', '')
-    if float(h) <= float(q):
-      a = amount[x]
-      h = amount[x].replace(',', '')
-      p = x
-  output = a + ' ' + naam[p]
-  print(a, naam[p])
-  #Maakt file
-  #f = open("LogBitcoinMost.txt", "a")
-  #f.write(output)
-  #f.write("\n")
-  #f.close()
-  
-  output = {"hash": naam[p] , "amount": a}
-  x = col_BitCoin.insert_one(output)
-
 
 
 while True:
-  datam =  []
-  dataa = []
+  datan =  [] #Data naam
+  dataa = [] #Data Amount
+  datab= [] #Data BitCoin
+  datat=[] #Data Time
   scrape()
   time.sleep(60)
