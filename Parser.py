23c1a60398554d48611e51c0ae66b1f0b79662dc
Femke-r0797855 <79083840+Femke-r0797855@users.noreply.github.com>
2021-03-26 21:27:13 +0100

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -4,29 +4,35 @@ import requests
 import pymongo as mongo
 import redis
 
-
 #MongoDB aanmaken
 client = mongo.MongoClient("mongodb://127.0.0.1:27017") #Connect to mongo db
 db_BitCoin = client["BitCoinTransactie"]
 col_BitCoin = db_BitCoin["BitCoin"]
 
 
-
-
-
-
-
-  h = 0
-  p = -1
-  a = 'IetsAnders'
-  for x in range(len(amount)):
-    q = amount[x].replace(',', '')
-    if float(h) <= float(q):
-      a = amount[x]
-      h = amount[x].replace(',', '')
-      p = x
-  output = a + ' ' + naam[p]
-  print(a, naam[p])
+#Gegevens uit Redis halen
+def parse():
+  highest = 0
+  plaats = -1
+  name = 'IetsAnders'
   
-  output = {"hash": naam[p] , "amount": a}
-  x = col_BitCoin.insert_one(output)
+  #print("Ik ben niet lui")
+  r = redis.Redis()
+  for key in r.keys():
+    naam = key
+    data = r.get(key)
+    x = data.split("-")
+    print()
+    
+    if float(highest)< x[0]:
+      name = naam
+      highest = x[0]
+    
+  print(str(name) + str(highest))
+  
+  
+#Timer
+while True:
+  x = []
+  parse()
+  time.sleep(60)
